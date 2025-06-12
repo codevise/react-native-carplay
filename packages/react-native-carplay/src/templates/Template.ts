1fb92aacd7d1f6b6d6d8a92283b4d7deac9b790b
Daniel Kuhn <dkuhn@codevise.de>
2025-06-12 13:50:26 +0200

    Fix/unify usage of resolveAssetSource

--- packages/react-native-carplay/src/templates/Template.ts
+++ packages/react-native-carplay/src/templates/Template.ts
@@ -1,9 +1,6 @@
-import { ImageSourcePropType } from 'react-native';
+import { Image, ImageSourcePropType } from 'react-native';
 import { CarPlay } from '../CarPlay';
 import { BarButton } from '../interfaces/BarButton';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource');
 
 export interface BaseEvent {
   /**
@@ -136,8 +133,7 @@ export class Template<P> {
           traverse(obj[i]);
         }
         if (String(i).match(/[Ii]mage$/)) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-          obj[i] = resolveAssetSource(obj[i]);
+          obj[i] = Image.resolveAssetSource(obj[i]);
         }
       }
     }
